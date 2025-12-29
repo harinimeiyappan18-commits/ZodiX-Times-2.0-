dd97924fc0d492ef212b872a25955f69aeacb09c
harinimeiyappan18-commits <harinimeiyappan18@gmail.com>
2025-12-29 19:48:14 +0530

    Set up nodemailer transport for email sending

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,1 +1,10 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: "APP_PASSWORD_HERE"
+  }
+});
 

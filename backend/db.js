a9279e515829fdcea985c2bdb9a35a9650e183f3
harinimeiyappan18-commits <harinimeiyappan18@gmail.com>
2025-12-29 20:39:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,24 +3,17 @@ export const db = new sqlite3.Database("./zodix.db");
 
 db.serialize(() => {
   db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT UNIQUE,
-      password TEXT,
-      role TEXT
-    )
-  `);
+  CREATE TABLE IF NOT EXISTS submissions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    content TEXT,
+    type TEXT,
+    source TEXT,
+    status TEXT,
+    originality INTEGER,
+    authorId INTEGER
+  )
+`);
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS submissions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      content TEXT,
-      type TEXT,
-      source TEXT,
-      status TEXT,
-      authorId INTEGER
-    )
-  `);
 });
 
